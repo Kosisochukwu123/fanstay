178a388ebadad71a375aba0d5995525eb2a6b4f2
Kosisochukwu123 <kosiogbunuko@gmail.com>
2026-06-15 15:33:48 +0100

    first 2

--- backend/src/config/coinbase.js
+++ backend/src/config/coinbase.js
@@ -1,6 +1,6 @@
 const coinbase = require('coinbase-commerce-node');
 const { Client } = coinbase;
 
-Client.init(process.env.COINBASE_COMMERCE_API_KEY);
+// Client.init(process.env.COINBASE_COMMERCE_API_KEY);
 
 module.exports = coinbase;
